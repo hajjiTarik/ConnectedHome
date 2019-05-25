f7c0fa00cbb26233aa58760e7eca4df2200f32fc
Hajji Tarik <thajji@sqli.com>
2019-05-25 15:35:21 +0000

    init login 2

--- App/Screens/Login/Components/LoginForm/index.js
+++ App/Screens/Login/Components/LoginForm/index.js
@@ -1,6 +1,9 @@
 import { View, Text, TextInput, TouchableOpacity ,StyleSheet ,StatusBar} from 'react-native';
 import { Redirect } from "react-router";
+import intl from "react-intl-universal";
 import React, {Component} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 import constantes from "../../../../constantes";
 
@@ -25,21 +28,25 @@ class LoginForm extends Component {
         return (
             <View style={styles.container}>
                 <StatusBar barStyle="light-content"/>
-                <Text></Text>
-                <TextInput style = {styles.input}
-                           autoCapitalize="none"
-                           onSubmitEditing={() => this.passwordInput.focus()}
-                           autoCorrect={false}
-                           keyboardType='email-address'
-                           returnKeyType="next"
-                           placeholder='Email or Mobile Num'
-                           placeholderTextColor='rgba(225,225,225,0.7)'/>
-
-                <TextInput style = {styles.input}
-                           returnKeyType="go" ref={(input)=> this.passwordInput = input}
-                           placeholder='Password'
-                           placeholderTextColor='rgba(225,225,225,0.7)'
-                           secureTextEntry/>
+                <Text>{intl.get("HOME_TEXT_MESSAGE")}</Text>
+                <View style={styles.input}>
+                    <FontAwesomeIcon icon={faLock} />
+                    <TextInput
+                               autoCapitalize="none"
+                               onSubmitEditing={() => this.passwordInput.focus()}
+                               autoCorrect={false}
+                               keyboardType='email-address'
+                               returnKeyType="next"
+                               placeholder='Email or Mobile Num'
+                               placeholderTextColor='rgba(225,225,225,0.7)'/>
+                </View>
+                <View style={styles.input}>
+                    <TextInput
+                               returnKeyType="go" ref={(input)=> this.passwordInput = input}
+                               placeholder='Password'
+                               placeholderTextColor='rgba(225,225,225,0.7)'
+                               secureTextEntry/>
+                </View>
                 <TouchableOpacity style={styles.buttonContainer} onPress={this.onButtonPress}>
                     <Text  style={styles.buttonText}>tTOTO</Text>
                 </TouchableOpacity>
@@ -54,10 +61,13 @@ const styles = StyleSheet.create({
     },
     input:{
         backgroundColor: constantes.colors.inputBgColor,
-        height: 40,
+        height: 55,
         marginBottom: 10,
-        padding: 10,
-        color: '#fff'
+        padding: 20,
+        color: '#fff',
+        borderRadius: 7,
+        fontSize: 18,
+        paddingLeft:60
     },
     buttonContainer:{
         backgroundColor: '#2980b6',
--- App/index.js
+++ App/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Provider} from "react-redux";
 import {NativeRouter} from "react-router-native";
-
+import intl from "react-intl-universal";
 import store from "./redux";
 import LoginPage from "./Screens/Login";
 import {StyleSheet, View} from "react-native";
@@ -9,19 +9,52 @@ import {Route} from "react-router";
 import Home from "./Screens/Home";
 import constantes from "./constantes";
 
-export default () => {
-    return(
-        <Provider store={store}>
-            <NativeRouter>
+const locales = {
+    fr_FR: require("./i18n/fr_FR.json")
+};
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            initDone: false
+        }
+    }
+    componentDidMount(){
+        this.loadLocales();
+    }
+
+    loadLocales = () => {
+        intl.init({
+            currentLocale: "fr_FR",
+            locales
+        }).then(() => {
+            this.setState({ initDone: true });
+        });
+    };
+
+    renderViews = () =>{
+        if (!this.state.initDone) return null;
+
+        return(
+            <View style={styles.container}>
+                <Route exact path="/" component={LoginPage} />
+                <Route path="/Home" component={Home} />
+            </View>
+        )
+    };
+
+    render(){
+        return(
+            <Provider store={store}>
                 <NativeRouter>
-                    <View style={styles.container}>
-                        <Route exact path="/" component={LoginPage} />
-                        <Route path="/Home" component={Home} />
-                    </View>
+                    <NativeRouter>
+                        {this.renderViews()}
+                    </NativeRouter>
                 </NativeRouter>
-            </NativeRouter>
-        </Provider>
-    )
+            </Provider>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -31,3 +64,5 @@ const styles = StyleSheet.create({
         paddingTop: 130
     }
 });
+
+export default App;
